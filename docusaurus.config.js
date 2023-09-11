b82cee48ff4a696072299093bdb6ec3dd9c1f127
rnawrozi <121869482+rnawrozi@users.noreply.github.com>
2023-09-11 10:02:20 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // ...
-  url: 'https://rnawrozi.github.io', // Your website URL
+  url: 'https://github.com/rnawrozi/rnawrozi.github.io', // Your website URL
   baseUrl: '/',
   projectName: 'rnawrozi.github.io',
   organizationName: 'rnawrozi',
