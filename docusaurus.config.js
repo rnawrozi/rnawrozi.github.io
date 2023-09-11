e1e80ae682cd64e07e9dc3fdb589671f9a6ad06f
rnawrozi <121869482+rnawrozi@users.noreply.github.com>
2023-09-11 09:26:38 +0200

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  url: 'https://rnawrozi.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'rnawrozi.github.io',
+  organizationName: 'rnawrozi',
+  trailingSlash: false,
+  // ...
+};
