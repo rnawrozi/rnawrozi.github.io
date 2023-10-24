f624850cbaaff505a2dd08a442d8d9d82e20d42f
rnawrozi <121869482+rnawrozi@users.noreply.github.com>
2023-10-24 19:06:10 +0200

    Delete docusaurus.config.js

--- docusaurus.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // ...
-  url: 'https://github.com/rnawrozi/rnawrozi.github.io', // Your website URL
-  baseUrl: '/',
-  projectName: 'rnawrozi.github.io',
-  organizationName: 'rnawrozi',
-  trailingSlash: false,
-  // ...
-};
